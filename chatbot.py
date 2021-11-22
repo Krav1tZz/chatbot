068dab922c9ef507e14a1c3a10eb5fd1d40910c5
Krav1tZz <90614426+Krav1tZz@users.noreply.github.com>
2021-11-22 06:45:35 +0200

    Create chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,12 +5,12 @@ def izglitibas_programmas():
     if izvele == 1:
         print(
             "Pirmsskolas izglītības iestāde ir dibināta 1961.gadā, Valmieras 2. vidusskolas paspārnē darbojas kopš 2000. gada. \nPirmsskolas izglītība ir obligāta bērniem no 5 gadu vecuma. \n"
-            "Mūsu pieredzējušie skolotāji palīdz skolēniem attīstīt pašapziņu, attīstīt fiziskās, intelektuālās un radošās prasmes, rūpēties par savu veselību un sagatavot tālākām mācībām \nPar ko vēl gribi uzzināt?")
-        return izvelne(vards)
+              "Mūsu pieredzējušie skolotāji palīdz skolēniem attīstīt pašapziņu, attīstīt fiziskās, intelektuālās un radošās prasmes, rūpēties par savu veselību un sagatavot tālākām mācībām \nPar ko vēl gribi uzzināt?")
+        return (izvelne(vards))
     elif izvele == 2:
-        print(
-            "Vispārējā izglītība pavisam ilgst 12 gadus, ietverot obligāto 9-gadīgo pamatizglītību un 3-gadīgo vidējo izglītību \nPar ko vēl gribi uzzināt?")
-        return izvelne(vards)
+       print(
+           "Vispārējā izglītība pavisam ilgst 12 gadus, ietverot obligāto 9-gadīgo pamatizglītību un 3-gadīgo vidējo izglītību \nPar ko vēl gribi uzzināt?")
+       return(izvelne(vards))
 
 
     else:
@@ -26,18 +26,18 @@ def direktori():
 def tradicijas():
     print(
         "Tradīcija ir tas, kas mums un mūsu kultūrai ir svarīgs. Dažas no mūsu skolas tradīcijām: Latvijas Valsts proklamēšanas diena, Zinību diena, Sporta svētki dažādās vecuma grupās. \nPar ko vēl gribi uzzināt?"
-        "\nJa vēlaties uzzināt vairāk par mūsu tradīcijām, sekojiet saitei http://v2v.edu.lv/par-skolu/tradicijas/")
-    return izvelne(vards)
+          "\nJa vēlaties uzzināt vairāk par mūsu tradīcijām, sekojiet saitei http://v2v.edu.lv/par-skolu/tradicijas/" )
+    return(izvelne(vards))
 
 
 def skolas_eka():
-    return
+     return
 
 
 def izvelne(vards):
     izvele = input(
         "1 - Izglitibas programmas \n2 - Direktori \n3 - Tradicijas \n4 - Skolas eka")
-    lietotaja_izvele = int(izvele)
+    lietotaja_izvele =  int(izvele)
     if lietotaja_izvele == 1:
         izglitibas_programmas()
     if lietotaja_izvele == 2:
@@ -47,12 +47,13 @@ def izvelne(vards):
     if lietotaja_izvele == 4:
         skolas_eka()
     else:
-        print("Izvēlieties 1-4")
-        izvelne(vards)
+         print("Izvēlieties 1-4")
+         izvelne(vards)
+
 
 
 def sasveicinaties(vards):
-    return print("Sveiki, " + vards)
+     return print("Sveiki, " + vards)
 
 
 if __name__ == '__main__':
